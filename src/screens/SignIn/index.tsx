f62137de61fd3094689009c524fbb6e316c5a5c4
root <augustoopieper@gmail.com>
2021-06-22 12:40:33 -0300

    Profile component/homescreen(WIP)

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,21 +3,15 @@ import {
     View,
     Text,
     Image,
-    StatusBar
 } from 'react-native'
 
-import { ButtonIcon } from '../../components/buttonIcon'
+import { ButtonIcon } from '../../components/ButtonIcon'
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 export function SignIn() {
     return (
         <View style={styles.container} >
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
             <Image
                 source={IllustrationImg}
                 style={styles.image}
@@ -25,12 +19,12 @@ export function SignIn() {
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas {`\n`}
-                    facilmente
+                    Conecte-se {'\n'}
+                    e organize suas {'\n'}
+                    jogatinas
                 </Text>
                 <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games {`\n`}
+                    Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos!
                 </Text>
                 <ButtonIcon
